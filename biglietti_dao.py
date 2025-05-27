770cce44fb24bf13f2fe9434a24ac3aa2bac060d
Adrian Boniolo <s300588@studenti.polito.it>
2025-05-27 19:09:56 +0200

    feat biglietti_dao

---
+++ biglietti_dao.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+# query che aggiunge un biglietto nel db
+def nuovo_biglietto(id_utente, tipo, giorni):
+
+    sql = "INSERT INTO biglietti (id_utente, tipo, giorni) VALUES (?, ?, ?)"
+
+    conn = sqlite3.connect("soundwave.db")
+    cursor = conn.cursor()
+    cursor.execute(sql, (id_utente, tipo, giorni))
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+
+# query che conta il numero di biglietti per giorno
+def count_biglietti_giornalieri(giorno):
+
+    sql = "SELECT COUNT(*) FROM biglietti WHERE giorni LIKE ?"
+
+    conn = sqlite3.connect("soundwave.db")
+    cursor = conn.cursor()
+    cursor.execute(sql, (f"%{giorno}%",))
+    count = cursor.fetchone()[0] # estrae prima colonna prima riga 
+
+    cursor.close()
+    conn.close()
+
+    return count
