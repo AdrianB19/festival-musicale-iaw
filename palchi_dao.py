62bff84e79d943b853d570ef7e386cfcc7918c4a
Adrian Boniolo <s300588@studenti.polito.it>
2025-05-27 18:46:50 +0200

    feat palchi_dao

---
+++ palchi_dao.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+# inserisco un nuovo palco
+def nuovo_palco(nome):
+
+    sql = "INSERT INTO palchi (nome) VALUES (?)"
+
+    conn = sqlite3.connect("soundwave.db")
+    cursor = conn.cursor()
+    cursor.execute(sql, (nome,))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+# ritorno tutti i palchi nel DB 
+def get_palchi():
+
+    sql = "SELECT * FROM palchi"
+
+    conn = sqlite3.connect("soundwave.db")
+    cursor = conn.cursor()
+    cursor.execute(sql)
+
+    palchi = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return palchi
+
+
+
+
