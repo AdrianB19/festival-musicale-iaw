5c0193d3791cf4c5e0fe0196d850777d60525654
Adrian Boniolo <s300588@studenti.polito.it>
2025-05-28 00:16:21 +0200

    feat flash

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
+# import necessari a visualizzazione delle routes, per l'autenticazione, per vedere i moduli
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 from flask_login import LoginManager, login_user, login_required, current_user
 
+from models  import User, Biglietto, Artista, Performance, Palco, Immagine
+
+import utenti_dao, biglietti_dao, artisti_dao, immagini_dao, performances_dao
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secretpass"
+
+#inizializzazione del login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route("/")
 def home():
@@ -12,3 +21,27 @@ def home():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+#processo i dati del form
+@app.route("/subscribe", methods=["POST"])
+def subscribe():
+    email = request.form.get('txt_email')
+    
+    if utenti_dao.get_utente_email(email):
+        flash("Email già registrata!", "error")
+        return redirect(url_for('signup'))
+    
+    utenti_dao.nuovo_utente(
+        request.form.get('txt_nome'),
+        request.form.get('txt_cognome'),
+        email,
+        request.form.get('txt_password'),
+        request.form.get('tipo')
+    )
+    
+    flash("Registrazione completata!", "success")
+    return redirect(url_for('login'))
